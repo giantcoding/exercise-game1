b17a6ff8e7be43eab763359f40a32129739915b4
Mike Morales <78278258+G14ANT@users.noreply.github.com>
2021-06-03 11:12:30 +0200

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -5,17 +5,18 @@ score = 0
 print("¡Bienvenido a mi juego de Python! ¿Cómo te llamas?")
 miNombre = input()
 
-número = random.randint(1, 20) # También se podría asignar un número fijo para que siempre sea el mismo.
-print("Bienvenido a mi juego en Python, " + miNombre + ", ¿Eres capaz de adivinar el número que estoy pensando? Es muy fácil, está entre 1 y 20")
+número = random.randint(1, 20) # Aquí se elige un número aleatorio del 1 al 20, se pueden cambiar valores.
+print("Hola, " + miNombre + ", ¿eres capaz de adivinar el número que estoy pensando? Es muy fácil, está entre 1 y 20")
 
 while score < 5:
+    print("Escribe el número.")
     elección = input()
     elección = int(elección)
 
     score = score + 1
 
     if elección < número:
-     print("Demasiado bajo, prueba un número más alto.")
+        print("Demasiado bajo, prueba un número más alto.")
     if elección > número:
         print ("Te has pasado, prueba otro más bajo.")
     if elección == número:
@@ -28,3 +29,5 @@ if elección == número:
 if elección!= número:
     número = str(número)
     print ("¡Has fallado! El número que pensaba era el " + número + ".")
+
+input("Presiona ENTER para salir")
